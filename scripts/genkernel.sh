728e68b5151d3427dabe8aee7aad3b4151fca2f9
mojyack <mojyack@gmail.com>
2022-04-23 16:16:29 +0900

    [genkernel.sh] add an ability to specity rootfs

--- scripts/genkernel.sh
+++ scripts/genkernel.sh
@@ -1,25 +1,37 @@
-#!/bin/bash
+#!/bin/zsh
 
 try() {
     $@ || { echo "genkernel: panic at command \"$@\""; exit 1; }
 }
 
+if (( $# >= 1 )) {
+    root="$1"
+    if [[ ! -d $root ]] {
+        echo "no such directory"
+        exit 1
+    }
+} else {
+    root=""
+}
+
+export INSTALL_PATH="$root/boot"
+export INSTALL_MOD_PATH="$root/"
 try make CC=clang LLVM=1 -j24
 try make modules_install
-try depmod $(echo "${PWD##*/}" | cut -d - -f 2-)
+try depmod -b "$root/" $(echo "${PWD##*/}" | cut -d - -f 2-)
 
-if [[ $(cat .config | grep CONFIG_EFI=)  == "CONFIG_EFI=y" ]]; then
+if [[ $(cat .config | grep CONFIG_EFI=)  == "CONFIG_EFI=y" ]] {
     try mkdir -p /boot/EFI/BOOT
     try mkdir -p /boot/EFI/Linux
-    if [[ -e /boot/EFI/BOOT/BOOTX64.EFI ]]; then
-        try mv /boot/EFI/BOOT/BOOTX64.EFI /boot/EFI/BOOT/BOOTX64.EFI.bak
-    fi
-    if [[ -e /boot/EFI/Linux/vmlinuz ]]; then
-        try mv /boot/EFI/Linux/vmlinuz /boot/EFI/Linux/vmlinuz.bak
-    fi
+    if [[ -e $root/boot/EFI/BOOT/BOOTX64.EFI ]] {
+        try mv "$root/boot/EFI/BOOT/BOOTX64.EFI" "$root/boot/EFI/BOOT/BOOTX64.EFI.bak"
+    }
+    if [[ -e $root/boot/EFI/Linux/vmlinuz ]] {
+        try mv "$root/boot/EFI/Linux/vmlinuz" "$root/boot/EFI/Linux/vmlinuz.bak"
+    }
     vmlinuz="arch/x86_64/boot/bzImage"
-    try cp $vmlinuz /boot/EFI/Linux/vmlinuz
-    try cp $vmlinuz /boot/EFI/BOOT/BOOTX64.EFI
-else
+    try cp $vmlinuz "$root/boot/EFI/Linux/vmlinuz"
+    try cp $vmlinuz "$root/boot/EFI/BOOT/BOOTX64.EFI"
+} else {
     try make install
-fi
+}
