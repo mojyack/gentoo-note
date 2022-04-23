72e4bb81c8d7f24533a416efe02fcac56ff027b2
mojyack <mojyack@gmail.com>
2022-04-23 16:17:04 +0900

    [rmkernel.sh] remove *.old efi files

--- scripts/rmkernel.sh
+++ scripts/rmkernel.sh
@@ -1,9 +1,13 @@
 #!/bin/zsh
 
 current=$(uname -r)
+
 for v ($(ls /lib/modules/)) {
     if [[ $current != $v ]]; then
         rm -rf /lib/modules/$v
         rm -rf /usr/src/linux-$v
     fi
 }
+
+rm -f "/boot/EFI/BOOT/BOOTX64.EFI.bak"
+rm -f "/boot/EFI/Linux/vmlinuz.bak"
