c706a9c3aef962728c3af8633e066d4836936a00
mojyack <mojyack@gmail.com>
2022-10-21 22:07:03 +0900

    update genkernel.sh

--- scripts/genkernel.sh
+++ scripts/genkernel.sh
@@ -16,22 +16,25 @@ if (( $# >= 1 )) {
 
 export INSTALL_PATH="$root/boot"
 export INSTALL_MOD_PATH="$root/"
+VERSION=$(echo "${PWD##*/}" | cut -d - -f 2-)
 try make CC=clang LLVM=1 -j24
 try make modules_install
-try depmod -b "$root/" $(echo "${PWD##*/}" | cut -d - -f 2-)
+try depmod -b "$root/" $VERSION
+
+vmlinuz="arch/x86_64/boot/bzImage"
 
 if [[ $(cat .config | grep CONFIG_EFI=)  == "CONFIG_EFI=y" ]] {
-    try mkdir -p /boot/EFI/BOOT
-    try mkdir -p /boot/EFI/Linux
+    try mkdir -p "$root/boot/EFI/BOOT"
+    try mkdir -p "$root/boot/EFI/Linux"
     if [[ -e $root/boot/EFI/BOOT/BOOTX64.EFI ]] {
         try mv "$root/boot/EFI/BOOT/BOOTX64.EFI" "$root/boot/EFI/BOOT/BOOTX64.EFI.bak"
     }
     if [[ -e $root/boot/EFI/Linux/vmlinuz ]] {
         try mv "$root/boot/EFI/Linux/vmlinuz" "$root/boot/EFI/Linux/vmlinuz.bak"
     }
-    vmlinuz="arch/x86_64/boot/bzImage"
     try cp $vmlinuz "$root/boot/EFI/Linux/vmlinuz"
     try cp $vmlinuz "$root/boot/EFI/BOOT/BOOTX64.EFI"
 } else {
-    try make install
+    try cp $vmlinuz "$root/boot/vmlinuz-$VERSION"
+    try ln -s -f vmlinuz-$VERSION "$root/boot/vmlinuz-current"
 }
