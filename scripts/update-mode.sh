d1090d44a16494b99f772484ab972e585adfa1a9
mojyack <mojyack@gmail.com>
2023-10-13 22:21:10 +0900

    update update-mode

--- scripts/update-mode.sh
+++ scripts/update-mode.sh
@@ -8,24 +8,22 @@ if [[ $UID != 0 ]]; then
 fi
 
 if [[ $1 == 1 ]]; then
-    mkdir -p /tmp/rtmp
-    mount solomon://var/cache/distfiles /var/cache/distfiles
-    mount solomon://tmp /tmp/rtmp
+    ip address add 192.168.10.2/24 dev $2
+    echo $2 > /tmp/upd-if
 
-    if [[ -n $2 ]]; then
-        ip address add 192.168.10.2/24 dev $2
-        echo $2 > /tmp/upd-if
-    fi
+    mkdir -p /tmp/rtmp
+    mount direct-host://var/cache/distfiles /var/cache/distfiles
+    mount direct-host://tmp /tmp/rtmp
+
     echo on
 elif [[ $1 == 0 ]]; then
+    ip address del 192.168.10.2/24 dev $(cat /tmp/upd-if)
+    rm /tmp/upd-if
+
     umount /var/cache/distfiles
     umount /tmp/rtmp
     rmdir /tmp/rtmp
 
-    if [[ -e /tmp/upd-if ]]; then
-        ip address del 192.168.10.2/24 dev $(cat /tmp/upd-if)
-        rm /tmp/upd-if
-    fi
     echo off
 else
     echo "unknown argument"
