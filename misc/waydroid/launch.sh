934c04ede41fd1909b65a400c0b1c59af9f9d084
mojyack <mojyack@gmail.com>
2023-12-29 18:33:51 +0900

    add waydroid memo

---
+++ misc/waydroid/launch.sh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+
+doas sed -i 's/lxc.apparmor.profile/#/' /var/lib/waydroid/lxc/waydroid/config
+doas sed -i 's/ro.hardware.vulkan/#/' /var/lib/waydroid/waydroid_base.prop
+
+doas systemctl restart waydroid-container.service
+waydroid session start &
+pid=$!
+waydroid show-full-ui &
+echo "android launched..."
+read
+kill $pid
+doas systemctl stop waydroid-container.service
