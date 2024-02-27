1ac3ae9626983e0917d69f08a1e7799fc9113590
mojyack <mojyack@gmail.com>
2024-02-27 19:06:51 +0900

    scripts/mount-client.sh: do not use showmount

--- scripts/mount-client.sh
+++ scripts/mount-client.sh
@@ -18,20 +18,12 @@ client="$1"
 mount_root="/tmp/mnt"
 mount="$mount_root/$client"
 
-boot=0
-showmount -e "$client" --no-headers | while read e; do
-    export=$(echo $e head -n1 | awk '{print "$1";}')
-    if [[ $export == "/boot" ]] {
-        boot=1
-        break
-    }
-done
-
 try mkdir -p "$mount"
 try mount -t nfs4 "$client":/ "$mount"
-if [[ $boot == 1 ]] {
-    try mount -t nfs4 "$client":/boot "$mount/boot"
-}
+# also try to mount /boot partition
+if ! mount -t nfs4 "$client":/boot "$mount/boot" 2> /dev/null; then
+    echo "/boot not found"
+fi
 
 echo "open"
 read
