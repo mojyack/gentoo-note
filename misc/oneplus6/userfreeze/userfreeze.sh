1a175889118f382c18a5ef7009c5937969eeeb36
mojyack <mojyack@gmail.com>
2024-06-16 22:51:59 +0900

    misc/oneplus6/userfreeze: ignore pipewire to workaround pop noises

--- misc/oneplus6/userfreeze/userfreeze.sh
+++ misc/oneplus6/userfreeze/userfreeze.sh
@@ -2,8 +2,13 @@
 
 trigger="/dev/input/by-path/platform-c440000.spmi-platform-c440000.spmi:pmic@0:pon@800:pwrkey-event"
 wait_bin=${0:a:h}/wait
+ignore_commands=(
+    # freezing pipewire sometimes causes audio glitches
+    pipewire
+    pipewire-pulse
+)
 
-targets=()
+stopped=()
 
 cd /proc
 for proc in */; do
@@ -11,18 +16,34 @@ for proc in */; do
     if [[ $pid != [[:digit:]]* || $pid == $$ ]]; then
         continue
     fi
-    read stat < $pid/stat
+
+    if ! pushd $pid >/dev/null 2>/dev/null; then
+        continue
+    fi
+
+    read stat < stat
     stat=( ${(z)stat} )
     state=${stat[3]}
     if [[ $state == "T" ]]; then
+        popd
         continue
     fi
-    targets=( $targets $pid )
+
+    read cmdline < cmdline
+    cmdline=${cmdline[1,-2]} # remove last null
+    if (( $ignore_commands[(Ie)$cmdline] != 0 )); then
+        popd
+        continue
+    fi
+
+    stopped=( $stopped $pid )
     kill -STOP $pid
+
+    popd
 done
 
 $wait_bin $trigger
 
-for pid in $targets; do
+for pid in $stopped; do
     kill -CONT $pid
 done
