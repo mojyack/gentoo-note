804eab95496bd0412cf63ebc9d4835b66843b6d0
mojyack <mojyack@gmail.com>
2024-09-02 18:16:01 +0900

    misc/oneplus6: migrate to daemonfs

--- misc/oneplus6/swclock/install.sh
+++ misc/oneplus6/swclock/install.sh
@@ -4,5 +4,5 @@ set -e
 
 cp rtc-read-offset /sbin
 cp rtc-save-offset /sbin
-cp 90-read-rtc-offset.sh /etc/runit/1.d
-cp 00-save-rtc-offset.sh /etc/runit/3.d
+cp 90-read-rtc-offset.sh /etc/init/1.d
+cp 00-save-rtc-offset.sh /etc/init/3.d
