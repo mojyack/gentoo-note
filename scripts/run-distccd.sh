480e4652083b6145cb27041ea93ab839c10ea268
mojyack <mojyack@gmail.com>
2026-08-18 13:51:45 +0900

    scripts: run-distccd.sh: cleanup

--- scripts/run-distccd.sh
+++ scripts/run-distccd.sh
@@ -19,21 +19,20 @@ else
 fi
 
 clang_base="$(realpath $(clang -print-resource-dir))"
-if [[ -z $llvm_slot ]]; then
-    llvm_slot=$(llvm-config --version | cut -d . -f 1)
-fi
+llvm_slot=${llvm_slot:-$(llvm-config --version | cut -d . -f 1)}
+workdir=$(realpath "distccd-$1-$llvm_slot")
 
 # setup cross tools
-mkdir -p "cross"
+mkdir "$workdir"
 
 # $1 clang/clang++
 create_file() {
     cc=$1
-    echo "#!/bin/sh\nexec \"$(command -v $cc-$llvm_slot || command -v $cc)\" --target=$tuple -Wno-gnu-line-marker \"\$@\"" > cross/$tuple-$cc
-    chmod +x "cross/$tuple-$cc"
-    ln -s "$tuple-$cc" "cross/$cc"
-    ln -s "$tuple-$cc" "cross/$cc-$llvm_slot"
-    ln -s "$tuple-$cc" "cross/$tuple-$cc-$llvm_slot"
+    echo "#!/bin/sh\nexec \"$(command -v $cc-$llvm_slot || command -v $cc)\" --target=$tuple -Wno-gnu-line-marker \"\$@\"" > $workdir/$tuple-$cc
+    chmod +x "$workdir/$tuple-$cc"
+    ln -s "$tuple-$cc" "$workdir/$cc"
+    ln -s "$tuple-$cc" "$workdir/$cc-$llvm_slot"
+    ln -s "$tuple-$cc" "$workdir/$tuple-$cc-$llvm_slot"
 }
 create_file clang
 create_file clang++
@@ -41,18 +40,19 @@ create_file clang++
 # run distccd
 args=(
     --daemon
-    --log-file=/tmp/distccd
+    --log-file=$workdir/log
     --enable-tcp-insecure
     --jobs 80
     --allow 127.0.0.1
     --allow 192.168.1.0/24
     --allow 192.168.128.0/22
 )
-PATH=$PWD/cross:$PATH exec distccd $args &
-echo ok
+PATH=$workdir:$PATH exec distccd $args &
+touch "$workdir/log"
+tail -f "$workdir/log" &
+
+echo ready
 read
-killall distccd
 
-rm -r cross
-
-exit 0
+pkill -f "$workdir"
+rm -r "$workdir"
