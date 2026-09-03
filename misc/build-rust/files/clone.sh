0cd2388910c15ead4a7e21cc7451b5c95184816e
mojyack <mojyack@gmail.com>
2026-09-03 12:00:45 +0900

    misc: build-rust: update commit hash

--- misc/build-rust/files/clone.sh
+++ misc/build-rust/files/clone.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 
 repo=$HOME/build/thirdparty/rust
 commit=31fca3adb283cc9dfd56b49cdee9a96eb9c96ffd  # 1.96.1
+commit=8bab26f4f68e0e26f0bb7960be334d5b520ea452  # 1.97.1
 
 if [[ ! -e $repo/.git ]]; then
     git clone --depth=10000 https://github.com/rust-lang/rust.git "$repo"
